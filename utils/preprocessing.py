53bb97ecf6050a84cdaef3f1fe9f41c0771b05ca
Auliya Istighfarina S <160805439+Auliyaaas@users.noreply.github.com>
2025-12-17 22:40:05 +0700

    update code

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,14 +1,24 @@
+import re
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.util import ngrams
 
-STOPWORDS = set(stopwords.words('english') + stopwords.words('indonesian'))
+def get_stopwords():
+    return set(stopwords.words('english') + stopwords.words('indonesian'))
 
 def sentences_from_text(text):
     return sent_tokenize(text)
 
-def build_terms(sentences):
-    words = word_tokenize(" ".join(sentences).lower())
-    unigrams = [w for w in words if w.isalpha() and len(w) > 1 and w not in STOPWORDS]
-    bigrams = [f"{a}_{b}" for a, b in ngrams(unigrams, 2)]
+def build_terms(text):
+    stop_words = get_stopwords()
+
+    words = word_tokenize(text.lower())
+    words = [
+        w for w in words
+        if w.isalpha() and len(w) > 1 and w not in stop_words
+    ]
+
+    unigrams = words
+    bigrams = [f"{a}_{b}" for a, b in ngrams(words, 2)]
+
     return unigrams + bigrams
